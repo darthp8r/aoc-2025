080f99413119f444982cccfd2baef664572395c2
darthp8r <darthp8r@gmail.com>
2025-12-03 22:59:31 +0000

    python/mamba environment

---
+++ python/spec/spec_helper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+def configure(settings):
+  # settings.slow_test_threshold = 0.075
+  # settings.enable_code_coverage = False
+  # settings.code_coverage_file = '.coverage'
+  settings.format = 'documentation'
+  # settings.no_color = False
+  # settings.tags = None
+
