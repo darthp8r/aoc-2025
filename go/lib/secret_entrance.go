f6c541973c21cbd9c117f2c69f5c6a8a29a61905
darthp8r <darthp8r@gmail.com>
2025-12-13 22:20:32 +0000

    "NewSecretEntrance" constructor idiom

--- go/lib/secret_entrance.go
+++ go/lib/secret_entrance.go
@@ -1,14 +1,51 @@
 package aoc2025
 
 import (
+	"strconv"
 	"fmt"
 )
 
-func SecretEntrance(turns []string) int {
-	//	repeat the list, again, to show I'm here
-	for i, turn := range turns {
-		fmt.Printf("(%02v) %c%s%c\n", i, '"', turn, '"')
+type SecretEntrance struct {
+	Turns []int
+}
+
+
+/*	Seems that idiomatic go is to have this New$(STRUCTNAME) constructor
+	function that takes in the dependent data and creates the structure
+	around whatever you do to said data, and returns a reference to same.
+*/
+func NewSecretEntrance(turns []string) *SecretEntrance {
+	me := &SecretEntrance {
+		Turns: make([]int, 0),
 	}
 
-	return len(turns)
+	for _, turn := range turns {
+		amount, _ := strconv.Atoi(turn[1:])
+		switch turn[0] {
+		case 'L':
+			me.Turns = append(me.Turns, amount * -1)
+		case 'R':
+			me.Turns = append(me.Turns, amount)
+		default:
+			me.Turns = append(me.Turns, 0)
+		}
+	}
+
+	return me
 }
+
+
+func (me SecretEntrance) Dunsel() int {
+	fmt.Println(me.Turns)
+
+	return len(me.Turns)
+}
+
+func (me SecretEntrance) Zeroes() int {
+	return len(me.Turns)
+}
+
+func (me SecretEntrance) Clicks() int {
+	return len(me.Turns)
+}
+
