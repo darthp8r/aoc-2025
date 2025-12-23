fde28f0d293ee3c72cb7ed752aaa097a3014a516
darthp8r <darthp8r@gmail.com>
2025-12-23 01:31:55 +0000

    moved from top-level support folder

---
+++ go/spec/support/file_to_list.go
@@ -0,0 +1,27 @@
+package aoc2025_spec_support
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+func FileToList(filename string) []string {
+	lyst := make([]string, 0)
+
+	file, oops := os.Open(filename)
+	if (oops == nil) {
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			text := strings.TrimSpace(scanner.Text())
+			if len(text) > 0 {
+				lyst = append(lyst, text)
+			}
+		}
+	}
+
+	return lyst
+}
+
