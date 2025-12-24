458e31feabdedd46f2c6d6dc2afece5925976558
darthp8r <darthp8r@gmail.com>
2025-12-24 04:08:06 +0000

    tell the compiler to build "spec/support" for :test

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -7,10 +7,15 @@ defmodule Aoc2025.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
+      deps: deps(),
       preferred_cli_env: [espec: :test],
-      deps: deps()
+      elixirc_paths: elixirc_paths(Mix.env()),
     ]
   end
+
+  # Which paths to compile per environment
+  defp elixirc_paths(:test), do: ["lib", "spec/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
