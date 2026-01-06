b2b7183d7311a94d43d650f096fa763fcd4dda06
darthp8r <darthp8r@gmail.com>
2026-01-06 22:32:48 +0000

    GiftShop, part one

---
+++ ruby/lib/aoc2025/gift_shop.rb
@@ -0,0 +1,74 @@
+module Aoc2025
+  class GiftShop
+    attr_reader :ranges
+
+    # this time we're told there is only one line of text
+    #
+    def initialize ranges=[]
+      @ranges = ranges[0].split(',').inject([]) do |memo, item|
+        memo.append item.split '-'
+        memo
+      end
+    end
+
+    def mirrored
+      @ranges.inject(0) do |memo, (lo, hi)|
+        memo +
+          if lo.length == hi.length
+            mirror(lo, hi)
+          elsif lo.length < hi.length
+            mirror(lo, "9" * lo.length) + mirror("1" + "0" * lo.length, hi)
+          else
+            0
+          end
+      end
+    end
+
+    private def mirror(lo, hi)
+      return 0 if lo.length.odd? or hi.length.odd?
+
+      magnitude = 10 ** (lo.length / 2)
+      lo = lo.to_i
+      hi = hi.to_i
+
+      lo_upper, lo_lower = (lo / magnitude), (lo % magnitude)
+      hi_upper, hi_lower = (hi / magnitude), (hi % magnitude)
+
+      # determine where to start incrementing
+      if lo_lower < lo_upper          # 84 promotes to 88
+        lo_lower = lo_upper
+      elsif lo_lower == lo_upper      # 66 remains
+        nil
+      else                            # 48 promotes to 55
+        lo_upper += 1
+        lo_lower = lo_upper
+      end
+
+      lo = (lo_upper * magnitude) + lo_lower
+      hi = (hi_upper * magnitude) + hi_lower
+      return 0 unless lo <= hi   # promoted the lower bound out of bounds
+
+      (lo..hi).step(1+magnitude).inject(0) do |memo, item|
+        memo + item
+      end
+    end
+
+    def repeated
+      @ranges.inject(0) do |memo, (lo, hi)|
+        memo +
+          if lo.length == hi.length
+            repeats(lo, hi)
+          elsif lo.length < hi.length
+            repeats(lo, "9" * lo.length) + repeats("1" + "0" * lo.length, hi)
+          else
+            0
+          end
+      end
+    end
+
+    private def repeats(lo, hi)
+      0
+    end
+
+  end
+end
---
+++ ruby/spec/aoc2025/day_02_spec.rb
@@ -0,0 +1,50 @@
+module Aoc2025
+  RSpec.describe GiftShop do
+    context "Sample" do
+      before(:all) do
+        @subject = GiftShop.new Support.file_to_list __FILE__.gsub('spec.rb', 'sample.txt')
+      end
+      subject { @subject }
+
+      describe "Part 1" do
+        it "sample data agrees" do
+          expect(subject.mirrored).to eq 1227775554
+        end
+      end
+
+      describe "Part 2" do
+        it "sample data agrees" do
+          expect(subject.repeated).to eq 4174379265
+        end
+      end
+    end
+
+    context "Actual" do
+      before(:all) do
+        @subject = GiftShop.new Support.file_to_list __FILE__.gsub('spec.rb', 'actual.txt')
+      end
+      subject { @subject }
+
+      describe "Part 1" do
+        it "actual answer above" do
+          pp subject.mirrored
+        end
+
+        it "results survive" do
+          expect(subject.mirrored).to eq 38158151648
+        end
+      end
+
+      describe "Part 2" ,:skip do
+        it "actual answer above" do
+          pp subject.repeated
+        end
+
+        it "results survive" do
+          expect(subject.repeated).to eq 6684
+        end
+      end
+    end
+  end
+end
+
