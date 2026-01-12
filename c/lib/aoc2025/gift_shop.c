902e2cc7789d30566f731c8ff4a83a3e94c5949a
darthp8r <darthp8r@gmail.com>
2026-01-12 22:56:22 +0000

    Chase 'const' removal in constructor

--- c/lib/aoc2025/gift_shop.c
+++ c/lib/aoc2025/gift_shop.c
@@ -14,16 +14,27 @@ repeated(const list_t *data)
   return 0;
 }
 
+/*  extract each comma-separated range from one line of text */
+static list_t *
+transform(list_t *data)
+{
+  return data;
+}
+
 
 aoc_t *
-gift_shop(const list_t *data)
+gift_shop(list_t *data)
 {
-  aoc_t noo = {
+  aoc_t me = {
     .part1 = mirrored,
     .part2 = repeated,
-    .data = data,
+    .data = transform(data),
   };
 
-  return memdup((void*) &noo, sizeof(noo));
+  for (register const list_t *node = me.data; node; node = node->next) {
+    printf("(%lu %lu)\n", node->u.nn[0], node->u.nn[1]);
+  }
+
+  return memdup((void*) &me, sizeof(me));
 }
 
