c9c98f8fd6483f0a575d926e66caf4b19c7b214b
darthp8r <darthp8r@gmail.com>
2026-01-12 22:56:07 +0000

    complete: ported from go

--- c/lib/aoc2025/secret_entrance.c
+++ c/lib/aoc2025/secret_entrance.c
@@ -4,26 +4,107 @@
 static size_t
 zeroes(const list_t *data)
 {
-  return 0;
+  register const list_t *node = NULL;
+
+  register long long int combination = 0;
+  register long long int position = 50;
+  register long long int turn = 0;
+
+  for (node = data; node; node = node->next) {
+    position += node->u.z;
+    position %= 100;
+    if (!position) {
+      combination++;
+    }
+  }
+
+  return combination;
 }
 
 
 static size_t
 clicks(const list_t *data)
 {
-  return 0;
+  register const list_t *node = NULL;
+
+  register long long int combination = 0;
+  register long long int position = 50;
+  register long long int landing = 0;
+  register long long int turn = 0;
+
+  for (node = data; node; node = node->next) {
+    turn = node->u.z;
+    if (turn < 0) {
+      combination -= turn / 100;
+    }
+    else {
+      combination += turn / 100;
+    }
+    landing = position + (turn % 100);
+
+    if (landing < 0) {
+      if (position) {
+        combination++;
+      }
+      position = landing + 100;
+    }
+    else if (!landing) {
+      combination++;
+      position = landing;
+    }
+    else if (landing < 100) {
+      position = landing;
+    }
+    else {
+      combination++;
+      position = landing - 100;
+    }
+  }
+
+  return combination;
 }
 
 
-aoc_t *
-secret_entrance(const list_t *data)
+/*  redefine each line to be an integer number, negative for left
+    turns, positive for right ones */
+static list_t *
+transform(list_t *data)
 {
-  aoc_t noo = {
+  register list_t *node = NULL;
+  register char direction = 0;
+
+  long long int turn = 0;
+
+  for (node = data; node; node = node->next) {
+    direction = node->u.str.text[0];
+    sscanf(1+node->u.str.text, "%lld", &turn);
+    free(node->u.str.text);
+
+    switch (direction) {
+    case 'L':
+      node->u.z = -turn;
+      break;
+    case 'R':
+      node->u.z = turn;
+      break;
+    default:
+      node->u.z = 0;
+      break;
+    }
+  }
+
+  return data;
+}
+
+aoc_t *
+secret_entrance(list_t *data)
+{
+  aoc_t me = {
     .part1 = zeroes,
     .part2 = clicks,
-    .data = data,
+    .data = transform(data),
   };
 
-  return memdup((void*) &noo, sizeof(noo));
+  return memdup((void*) &me, sizeof(me));
 }
 
