8b968a1c4cd6995dbde7b6308481076ca14f8f44
darthp8r <darthp8r@gmail.com>
2026-01-12 20:04:35 +0000

    answers known so far

---
+++ c/spec/aoc2025/day_01_spec.c
@@ -0,0 +1,54 @@
+#include <aoc2025.h>
+
+#include "file_to_list.h"
+
+#include "bdd-for-c.h"
+
+spec("Secret Entrance") {
+  static aoc_t *subject = NULL;
+
+  context("Sample") {
+    before() {
+      subject = secret_entrance(file_to_list("spec/aoc2025/day_01_sample.txt"));
+    }
+
+    describe("Part 1") {
+      it("sample data agrees") {
+        check(subject->part1(subject->data) == 3);
+      }
+    }
+
+    describe("Part 2") {
+      it("sample data agrees") {
+        check(subject->part2(subject->data) == 6);
+      }
+    }
+  }
+
+  context("Actual") {
+    before() {
+      subject = secret_entrance(file_to_list("spec/aoc2025/day_01_actual.txt"));
+    }
+
+    describe("Part 1") {
+      it("actual answer ==>") {
+        printf("%lu\n", subject->part1(subject->data));
+      }
+
+      it("results survive") {
+        check(subject->part1(subject->data) == 1139);
+      }
+    }
+
+    describe("Part 2") {
+      it("actual answer ==>") {
+        printf("%lu\n", subject->part2(subject->data));
+      }
+
+      it("results survive") {
+        check(subject->part2(subject->data) == 6684);
+      }
+    }
+  }
+}
+
---
+++ c/spec/aoc2025/day_02_spec.c
@@ -0,0 +1,54 @@
+#include <aoc2025.h>
+
+#include "file_to_list.h"
+
+#include "bdd-for-c.h"
+
+spec("Gift Shop") {
+  static aoc_t *subject = NULL;
+
+  context("Sample") {
+    before() {
+      subject = gift_shop(file_to_list("spec/aoc2025/day_02_sample.txt"));
+    }
+
+    describe("Part 1") {
+      it("sample data agrees") {
+        check(subject->part1(subject->data) == 1227775554);
+      }
+    }
+
+    describe("Part 2") {
+      it("sample data agrees") {
+        check(subject->part2(subject->data) == 4174379265);
+      }
+    }
+  }
+
+  context("Actual") {
+    before() {
+      subject = gift_shop(file_to_list("spec/aoc2025/day_02_actual.txt"));
+    }
+
+    describe("Part 1") {
+      it("actual answer ==>") {
+        printf("%lu\n", subject->part1(subject->data));
+      }
+
+      it("results survive") {
+        check(subject->part1(subject->data) == 38158151648);
+      }
+    }
+
+    describe("Part 2") {
+      it("actual answer ==>") {
+        printf("%lu\n", subject->part2(subject->data));
+      }
+
+      xit("results survive") {
+        check(subject->part2(subject->data) == 6684);
+      }
+    }
+  }
+}
+
