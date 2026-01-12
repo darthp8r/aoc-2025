3b730f7a13c9f8ef5e5ccf378deb5ebe1029fda9
darthp8r <darthp8r@gmail.com>
2026-01-12 20:06:00 +0000

    list and challenge structures, conveniences, and the first two challenges

---
+++ c/include/aoc2025.h
@@ -0,0 +1,39 @@
+#ifndef __AOC2025__
+#define __AOC2025__
+
+#include <sys/types.h>
+
+/*  linked list of non-empty lines of strings */
+typedef struct list_s {
+  char          *text;
+  size_t        strlen;
+  struct list_s *next;
+} list_t;
+
+
+/*  usage: aoc->part1(aoc->data), aoc->part2(aoc->data) */
+typedef struct aoc_s {
+  size_t  (*part1)(const list_t *data);
+  size_t  (*part2)(const list_t *data);
+  const list_t *data;
+} aoc_t;
+
+
+/*  print 'text' with a glow about it */
+int
+shine(const char *text);
+
+
+/*  a familiar toy of mine, for assembling a data structure on the stack
+    and then returning a clone of it from the heap */
+void *
+memdup(const void *source, size_t length);
+
+
+/*  all the challenges */
+aoc_t *secret_entrance(const list_t *data);
+aoc_t *gift_shop(const list_t *data);
+
+
+#endif
+
