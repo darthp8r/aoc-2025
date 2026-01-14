3cb01044108509166d5d363157580a81c560c86b
darthp8r <darthp8r@gmail.com>
2026-01-14 03:47:30 +0000

    examples need modern libraries to run against

--- c/spec/makeinc.mk
+++ c/spec/makeinc.mk
@@ -1,4 +1,4 @@
 
-$(EXAMPLES): %_spec: %_spec.c $(SUPPORT)
+$(EXAMPLES): %_spec: %_spec.c $(SUPPORT) $(LIBRARY)
 	$(CC) -Iinclude -Ispec/support -Ibdd-for-c -o $@ $< -Llib -Lspec/support -l$(NAME) -l$(NAME)spec
 
