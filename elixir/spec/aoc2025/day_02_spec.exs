05727ce7bf5d413bb8007db43e59cfbea3fbde1a
darthp8r <darthp8r@gmail.com>
2026-02-02 05:29:18 +0000

    untangle copy pasta; disable part 2

--- elixir/spec/aoc2025/day_02_spec.exs
+++ elixir/spec/aoc2025/day_02_spec.exs
@@ -1,51 +1,51 @@
 require Aoc2025.GiftShop
 
-defmodule Aoc2025Spec.Day01 do
+defmodule Aoc2025Spec.Day02 do
   use ESpec
 
   context "Sample" do
-    subject(Aoc2025.GiftShop.secret_entrance(
+    subject(Aoc2025.GiftShop.gift_shop(
       Aoc2025Spec.Support.file_to_list String.replace(__ENV__.file, "_spec.exs", "_sample.txt")))
 
     describe "Part 1" do
       it "sample data agrees" do
-        subject = Aoc2025Spec.Day01.subject
+        subject = Aoc2025Spec.Day02.subject
         expect(Aoc2025.GiftShop.mirrored(subject)) |> to(eq 1227775554)
       end
     end
 
-    describe "Part 2" do
+    xdescribe "Part 2" do
       it "sample data agrees" do
-        subject = Aoc2025Spec.Day01.subject
+        subject = Aoc2025Spec.Day02.subject
         expect(Aoc2025.GiftShop.repeated(subject)) |> to(eq 4174379265)
       end
     end
   end
 
   context "Actual" do
-    subject(Aoc2025.GiftShop.secret_entrance(
+    subject(Aoc2025.GiftShop.gift_shop(
       Aoc2025Spec.Support.file_to_list String.replace(__ENV__.file, "_spec.exs", "_actual.txt")))
 
     describe "Part 1" do
       it "actual answer above" do
-        subject = Aoc2025Spec.Day01.subject
+        subject = Aoc2025Spec.Day02.subject
         IO.puts Aoc2025.GiftShop.mirrored(subject)
       end
 
       it "results survive" do
-        subject = Aoc2025Spec.Day01.subject
+        subject = Aoc2025Spec.Day02.subject
         expect(Aoc2025.GiftShop.mirrored(subject)) |> to(eq 38158151648)
       end
     end
 
-    describe "Part 2" do
+    xdescribe "Part 2" do
       it "actual answer above" do
-        subject = Aoc2025Spec.Day01.subject
+        subject = Aoc2025Spec.Day02.subject
         IO.puts Aoc2025.GiftShop.repeated(subject)
       end
 
       it "results survive" do
-        subject = Aoc2025Spec.Day01.subject
+        subject = Aoc2025Spec.Day02.subject
         expect(Aoc2025.GiftShop.repeated(subject)) |> to(eq 45283684555)
       end
     end
